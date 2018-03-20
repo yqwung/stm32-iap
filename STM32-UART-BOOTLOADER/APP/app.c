124714a529cba4c94baf3ed841b3fcb7318a5dbc
yqwung <1522943999@qq.com>
2018-03-20 15:31:27 +0800

    APP添加喂狗函数

--- STM32-UART-BOOTLOADER/APP/app.c
+++ STM32-UART-BOOTLOADER/APP/app.c
@@ -145,42 +145,42 @@ int main(void)
         // 跳转到APP------------------------------------------------------------------------------------------------ 
         if( toApp == 1 )                   // 跳转到APP标志
         {
-            toApp = 0;
-            //-------------------------------------------------------USART Test----------------------------------------------------------//
-            BSP_USART1_Printf("----Jump_To_Application!\r\n");
-            BSP_USART2_Printf("----Jump_To_Application!\r\n");
-            BSP_USART3_Printf("----Jump_To_Application!\r\n");
-            BSP_USART4_Printf("----Jump_To_Application!\r\n");
-            
-            USART_Cmd(USART1, DISABLE);    // 失能串口
-            USART_DeInit(USART1);
-            USART_Cmd(USART2, DISABLE);    // 失能串口
-            USART_DeInit(USART2);
-            USART_Cmd(USART3, DISABLE);    // 失能串口
-            USART_DeInit(USART3);
-            USART_Cmd(UART4, DISABLE);     // 失能串口
-            USART_DeInit(UART4);
-            TIM_Cmd(TIM3, DISABLE);        // 失能定时器
-            TIM_DeInit(TIM3);              // 清除定时器配置
-            
-            RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOC |
-                                    RCC_APB2Periph_GPIOD | RCC_APB2Periph_GPIOE | RCC_APB2Periph_GPIOF |
-                                    RCC_APB2Periph_GPIOG,  DISABLE);
-            //RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, DISABLE);
-                       
-            ppfifo_free(usart1_ppfifo);    // 释放串口申请的空间
-            vfifo_free(usart1_vfifo);      
-            ppfifo_free(usart2_ppfifo);    // 释放串口申请的空间
-            vfifo_free(usart2_vfifo);   
-            ppfifo_free(usart3_ppfifo);    // 释放串口申请的空间
-            vfifo_free(usart3_vfifo);   
-            ppfifo_free(usart4_ppfifo);    // 释放串口申请的空间
-            vfifo_free(usart4_vfifo);
-            
-	    BSP_IWDG_Init(1600);           // 初始计数值9s            
-            __set_PRIMASK(1);              // 关总中断
-            
-            iap_to_app();                  // 跳转到APP执行
+			toApp = 0;
+			//-------------------------------------------------------USART Test----------------------------------------------------------//
+			BSP_USART1_Printf("---Jump_To_Application!\r\n");
+			BSP_USART2_Printf("---Jump_To_Application!\r\n");
+			BSP_USART3_Printf("---Jump_To_Application!\r\n");
+			BSP_USART4_Printf("---Jump_To_Application!\r\n");
+
+			USART_Cmd(USART1, DISABLE);    // 失能串口
+			USART_DeInit(USART1);
+			USART_Cmd(USART2, DISABLE);    // 失能串口
+			USART_DeInit(USART2);
+			USART_Cmd(USART3, DISABLE);    // 失能串口
+			USART_DeInit(USART3);
+			USART_Cmd(UART4, DISABLE);     // 失能串口
+			USART_DeInit(UART4);
+			TIM_Cmd(TIM3, DISABLE);        // 失能定时器
+			TIM_DeInit(TIM3);              // 清除定时器配置
+
+			RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOC |
+			RCC_APB2Periph_GPIOD | RCC_APB2Periph_GPIOE | RCC_APB2Periph_GPIOF |
+			RCC_APB2Periph_GPIOG,  DISABLE);
+			//RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, DISABLE);
+
+			ppfifo_free(usart1_ppfifo);    // 释放串口申请的空间
+			vfifo_free(usart1_vfifo);      
+			ppfifo_free(usart2_ppfifo);    // 释放串口申请的空间
+			vfifo_free(usart2_vfifo);   
+			ppfifo_free(usart3_ppfifo);    // 释放串口申请的空间
+			vfifo_free(usart3_vfifo);   
+			ppfifo_free(usart4_ppfifo);    // 释放串口申请的空间
+			vfifo_free(usart4_vfifo);
+
+			BSP_IWDG_Init(1600);           // 初始计数值9s            
+			__set_PRIMASK(1);              // 关总中断
+
+			iap_to_app();                  // 跳转到APP执行
         }       
     }
 }
